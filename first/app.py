6ed1d4c84fb14ac7f947d932c38bf1321e86f927
daria <128214762+darieaufg23@users.noreply.github.com>
2024-02-19 21:03:24 +0100

    Add files via upload

---
+++ first/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('first_page.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
