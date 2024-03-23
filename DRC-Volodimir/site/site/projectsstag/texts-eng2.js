42d4ad57e22ee72805bd23ed773171f6cad941e4
Bulefrg <143168692+Bulefrg@users.noreply.github.com>
2024-03-23 21:59:35 +0200

    Add files via upload

---
+++ DRC-Volodimir/site/site/projectsstag/texts-eng2.js
@@ -0,0 +1,14 @@
+const textsEng = {
+  h1: "DEVELOPMENT IN PROGRESS",
+  "p-1":
+    "Development of the news resource DRC-Inform is still ongoing. Fill out the form below if you want to receive release notifications.",
+  name_placeholder: "Your name",
+  email_placeholder: "Email",
+  submit_button: "KEEP ME UPDATED",
+  facebook_button: "Facebook",
+  telegram_button: "Telegram",
+  sms_button: "SMS",
+  copyright_text: "© Copyright 2024",
+};
+
+module.exports = textsEng;
