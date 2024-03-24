8db10b5f44d67acc4154c754c54bf2aa2e05cf27
Bulefrg <143168692+Bulefrg@users.noreply.github.com>
2024-03-24 16:15:10 +0200

    Add files via upload

---
+++ Statut/site/site/projectsstag/texts-ukr2.js
@@ -0,0 +1,14 @@
+const textsUkr = {
+  h1: "РОЗРОБКА ТРИВАЄ",
+  "p-1":
+    "Розробка новинного ресурсу DRC-Inform досі триває. Заповніть форму нижче, якщо хочете отримати повідомлення про реліз.",
+  name_placeholder: "Ваше ім’я",
+  email_placeholder: "Електронна пошта",
+  submit_button: "ТРИМАЙТЕ МЕНЕ В КУРСІ",
+  facebook_button: "Facebook",
+  telegram_button: "Telegram",
+  sms_button: "SMS",
+  copyright_text: "© Авторські права 2024",
+};
+
+module.exports = textsUkr;
